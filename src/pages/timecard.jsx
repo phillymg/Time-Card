b9614cc4eb2406eb10fe1d5e723feeaea0ad59a3
Philmg <phillipmguetterman@att.net>
2023-12-22 14:59:27 -0600

    add and subtract all cells

--- src/pages/timecard.jsx
+++ src/pages/timecard.jsx
@@ -10,13 +10,71 @@ const date = today.getDate();
 const currentDate = month + "/" + date + "/" + year;
 
 function mondayTotal() {
-    const timeIn = document.getElementById('mondayTimeIn').value || 0;
-    const timeOut = document.getElementById('mondayTimeOut').value || 0;
-    const totalTime = timeOut - timeIn;
+    const timeIn = parseFloat(document.getElementById('mondayTimeIn').value) || 0;
+    const timeOut = parseFloat(document.getElementById('mondayTimeOut').value) || 0;
+    const lunch = parseFloat(document.getElementById('mondayLunch').value) || 0;
+    const pto = parseFloat(document.getElementById('mondayPTO').value) || 0;
+    const totalTime = timeOut - timeIn - lunch;
+    const newTotal = totalTime + pto;
     document.getElementById("totalMond").innerHTML = totalTime;
+    document.getElementById("newMond").innerHTML = newTotal;
 }
 
-mondayTotal();
+
+function tuesdayTotal() {
+    const timeIn = parseFloat(document.getElementById('tuesdayTimeIn').value) || 0;
+    const timeOut = parseFloat(document.getElementById('tuesdayTimeOut').value) || 0;
+    const lunch = parseFloat(document.getElementById('tuesdayLunch').value) || 0;
+    const pto = parseFloat(document.getElementById('tuesdayPTO').value) || 0;
+    const totalTime = timeOut - timeIn - lunch;
+    const newTotal = totalTime + pto;
+    document.getElementById("totalTues").innerHTML = totalTime;
+    document.getElementById("newTues").innerHTML = newTotal;
+
+}
+
+function wednesdayTotal() {
+    const timeIn = parseFloat(document.getElementById('wednesdayTimeIn').value) || 0;
+    const timeOut = parseFloat(document.getElementById('wednesdayTimeOut').value) || 0;
+    const lunch = parseFloat(document.getElementById('wednesdayLunch').value) || 0;
+    const pto = parseFloat(document.getElementById('wednesdayPTO').value) || 0;
+    const totalTime = timeOut - timeIn - lunch;
+    const newTotal = totalTime + pto;
+    document.getElementById("totalWed").innerHTML = totalTime;
+    document.getElementById("newWed").innerHTML = newTotal;
+
+}
+
+function thursdayTotal() {
+    const timeIn = parseFloat(document.getElementById('thursdayTimeIn').value) || 0;
+    const timeOut = parseFloat(document.getElementById('thursdayTimeOut').value) || 0;
+    const lunch = parseFloat(document.getElementById('thursdayLunch').value) || 0;
+    const pto = parseFloat(document.getElementById('thursdayPTO').value) || 0;
+    const totalTime = timeOut - timeIn - lunch;
+    const newTotal = totalTime + pto;
+    document.getElementById("totalThur").innerHTML = totalTime;
+    document.getElementById("newThur").innerHTML = newTotal;
+}
+
+function fridayTotal() {
+    const timeIn = parseFloat(document.getElementById('fridayTimeIn').value) || 0;
+    const timeOut = parseFloat(document.getElementById('fridayTimeOut').value) || 0;
+    const lunch = parseFloat(document.getElementById('fridayLunch').value) || 0;
+    const pto = parseFloat(document.getElementById('fridayPTO').value) || 0;
+    const totalTime = timeOut - timeIn - lunch;
+    const newTotal = totalTime + pto;
+    document.getElementById("totalFri").innerHTML = totalTime;
+    document.getElementById("newFri").innerHTML = newTotal;
+
+}
+
+function calculateAll() {
+    mondayTotal();
+    tuesdayTotal();
+    wednesdayTotal();
+    thursdayTotal();
+    fridayTotal();
+}
 
 export default function TimeCard() {
 
@@ -49,132 +107,124 @@ export default function TimeCard() {
                     </td>
                     <td>
                         <InputGroup>
-                            <Form.Control></Form.Control>
+                            <Form.Control id="mondayLunch"></Form.Control>
                         </InputGroup>
                     </td>
                     <td id="totalMond">total</td>
                     <td>
                         <InputGroup>
-                            <Form.Control></Form.Control>
+                            <Form.Control id="mondayPTO"></Form.Control>
                         </InputGroup>
                     </td>
-                    <td></td>
+                    <td id="newMond"></td>
                     <td></td>
                 </tr>
                 <tr>
                     <td>Tuesday</td>
                     <td>
                         <InputGroup>
-                            <Form.Control></Form.Control>
+                            <Form.Control id="tuesdayTimeIn"></Form.Control>
                         </InputGroup>
                     </td>
                     <td>
                         <InputGroup>
-                            <Form.Control></Form.Control>
+                            <Form.Control id="tuesdayTimeOut"></Form.Control>
                         </InputGroup>
                     </td>
                     <td>
                         <InputGroup>
-                            <Form.Control></Form.Control>
+                            <Form.Control id="tuesdayLunch"></Form.Control>
                         </InputGroup>
                     </td>
-                    <td>
-
-                    </td>
+                    <td id="totalTues"></td>
                     <td>
                         <InputGroup>
-                            <Form.Control></Form.Control>
+                            <Form.Control id="tuesdayPTO"></Form.Control>
                         </InputGroup>
                     </td>
-                    <td></td>
+                    <td id="newTues"></td>
                     <td></td>
                 </tr>
                 <tr>
                     <td>Wednesday</td>
                     <td>
                         <InputGroup>
-                            <Form.Control></Form.Control>
+                            <Form.Control id="wednesdayTimeIn"></Form.Control>
                         </InputGroup>
                     </td>
                     <td>
                         <InputGroup>
-                            <Form.Control></Form.Control>
+                            <Form.Control id="wednesdayTimeOut"></Form.Control>
                         </InputGroup>
                     </td>
                     <td>
                         <InputGroup>
-                            <Form.Control></Form.Control>
+                            <Form.Control id="wednesdayLunch"></Form.Control>
                         </InputGroup>
                     </td>
-                    <td>
-
-                    </td>
+                    <td id="totalWed"></td>
                     <td>
                         <InputGroup>
-                            <Form.Control></Form.Control>
+                            <Form.Control id="wednesdayPTO"></Form.Control>
                         </InputGroup>
                     </td>
-                    <td></td>
+                    <td id="newWed"></td>
                     <td></td>
                 </tr>
                 <tr>
                     <td>Thursday</td>
                     <td>
                         <InputGroup>
-                            <Form.Control></Form.Control>
+                            <Form.Control id="thursdayTimeIn"></Form.Control>
                         </InputGroup>
                     </td>
                     <td>
                         <InputGroup>
-                            <Form.Control></Form.Control>
+                            <Form.Control id="thursdayTimeOut"></Form.Control>
                         </InputGroup>
                     </td>
                     <td>
                         <InputGroup>
-                            <Form.Control></Form.Control>
+                            <Form.Control id="thursdayLunch"></Form.Control>
                         </InputGroup>
                     </td>
-                    <td>
-
-                    </td>
+                    <td id="totalThur"></td>
                     <td>
                         <InputGroup>
-                            <Form.Control></Form.Control>
+                            <Form.Control id="thursdayPTO"></Form.Control>
                         </InputGroup>
                     </td>
-                    <td></td>
+                    <td id="newThur"></td>
                     <td></td>
                 </tr>
                 <tr>
                     <td>Friday</td>
                     <td>
                         <InputGroup>
-                            <Form.Control></Form.Control>
+                            <Form.Control id="fridayTimeIn"></Form.Control>
                         </InputGroup>
                     </td>
                     <td>
                         <InputGroup>
-                            <Form.Control></Form.Control>
+                            <Form.Control id="fridayTimeOut"></Form.Control>
                         </InputGroup>
                     </td>
                     <td>
                         <InputGroup>
-                            <Form.Control></Form.Control>
+                            <Form.Control id="fridayLunch"></Form.Control>
                         </InputGroup>
                     </td>
-                    <td>
-
-                    </td>
+                    <td id="totalFri"></td>
                     <td>
                         <InputGroup>
-                            <Form.Control></Form.Control>
+                            <Form.Control id="fridayPTO"></Form.Control>
                         </InputGroup>
                     </td>
-                    <td></td>
+                    <td id="newFri"></td>
                     <td></td>
                 </tr>
             </tbody>
-            <Button onClick={mondayTotal}>Calculate</Button>
+            <Button onClick={calculateAll}>Calculate</Button>
 
         </Table >
     );
